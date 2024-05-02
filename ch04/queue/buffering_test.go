2dc26c09827dc3bb2092324a323f6f2936581cc6
Koichi Kaneshige <coarse.ground@gmail.com>
2024-05-02 22:58:50 +0900

    4.11 queue 2

---
+++ ch04/queue/buffering_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"testing"
+)
+
+func BenchmarkUnbufferedWrite(b *testing.B) {
+	performWrite(b, tmpFileOrFatal())
+}
+
+func BenchmarkBufferedWrite(b *testing.B) {
+	f := bufio.NewWriter(tmpFileOrFatal())
+	performWrite(b, f)
+}
+
+func tmpFileOrFatal() *os.File {
+	file, err := os.CreateTemp("", "tmp")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return file
+}
+
+func performWrite(b *testing.B, writer io.Writer) {
+	done := make(chan any)
+	defer close(done)
+
+	b.ResetTimer()
+	for bt := range take(done, repeat(done, byte(0)), b.N) {
+		writer.Write([]byte{bt.(byte)})
+	}
+}
